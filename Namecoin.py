f8de661b7d8abacc85a88d70ad63184e93437fdb
wordos <wordos520@qq.com>
2014-02-13 11:53:51 +0800

    Create Namecoin.py

---
+++ Namecoin.py
@@ -0,0 +1,50 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from . import data
+from p2pool.util import math, pack
+
+nets = dict(
+    namecoin=math.Object(
+        P2P_PREFIX='f9beb4fe'.decode('hex'),
+        P2P_PORT=8334,
+        ADDRESS_VERSION=52,
+        RPC_PORT=8332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'namecoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=600, # s
+        SYMBOL='NMC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Namecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Namecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.namecoin'), 'bitcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.dot-bit.org/b/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.dot-bit.org/a/',
+        SANE_TARGET_RANGE=(2**256//2**32 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+    ),
+    namecoin_testnet=math.Object(
+        P2P_PREFIX='fabfb5fe'.decode('hex'),
+        P2P_PORT=18334,
+        ADDRESS_VERSION=111,
+        RPC_PORT=8332,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'namecoinaddress' in (yield bitcoind.rpc_help()) and
+            (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 50*100000000 >> (height + 1)//210000,
+        POW_FUNC=data.hash256,
+        BLOCK_PERIOD=600, # s
+        SYMBOL='tNMC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Namecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Namecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.namecoin'), 'bitcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://testnet.explorer.dot-bit.org/b/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://testnet.explorer.dot-bit.org/a/',
+        SANE_TARGET_RANGE=(2**256//2**32 - 1, 2**256//2**32 - 1),
+        DUMB_SCRYPT_DIFF=1,
+    ),
+)
+for net_name, net in nets.iteritems():
+    net.NAME = net_name
